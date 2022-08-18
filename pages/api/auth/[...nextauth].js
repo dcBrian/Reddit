18bd27a08a059e81b7be23c86726cab169db7cc2
dev <ivucady@gmail.com>
2022-08-18 09:44:09 +0900

    add AUTH url variable

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import RedditProvider from "next-auth/providers/reddit";
 
 export default NextAuth({
+  site: process.env.NEXTAUTH_URL,
   // Configure one or more authentication providers
   providers: [
     RedditProvider({
